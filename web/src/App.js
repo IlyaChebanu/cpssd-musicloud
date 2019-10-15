c37667dbe0e3588a10cf36dc7635ef1e5c3cf139
Ilie Cebanu <cebanu5665@gmail.com>
2019-10-15 23:58:15 +0100

    File structure for login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Spicy from './components/Spicy/Spicy';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Spicy></Spicy>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- web/src/components/Spicy/Spicy.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Spicy.module.scss";
-
-export default props => {
-    return (
-        <p className={styles.spicy}>Spicy test with style!</p>
-    )
-}
