9c0bb8c544e240faf4556e363c25f96935e28ab4
Ilie Cebanu <cebanu5665@gmail.com>
2019-10-15 23:48:41 +0100

    add spicy component to test css modules

---
+++ web/src/components/Spicy/Spicy.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Spicy.module.scss";
+
+export default props => {
+    return (
+        <p className={styles.spicy}>Spicy test with style!</p>
+    )
+}
