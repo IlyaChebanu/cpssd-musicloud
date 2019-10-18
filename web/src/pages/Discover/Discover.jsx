d8f2ce171db74cf665299fb17149bac4872437b8
Ilie Cebanu <cebanu5665@gmail.com>
2019-10-18 01:38:24 +0100

    Music search component

--- web/src/pages/Discover/Discover.jsx
+++ web/src/pages/Discover/Discover.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Discover.module.scss';
 import Header from '../../components/Header';
+import MusicSearch from '../../components/MusicSearch';
 
 const Discover = props => {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <div className={styles.contentWrapper}>
+        <MusicSearch className={styles.musicSearch}/>
+      </div>
     </div>
   );
 };
