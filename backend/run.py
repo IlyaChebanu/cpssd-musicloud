cc34a97ed5fa43d1a292ecbbfb0d9bfc079a347f
Rawd2020 <rawd2020personal@gmail.com>
2019-10-19 16:20:30 +0100

    12: Fixed the issue preventing communication with the container.

--- backend/run.py
+++ backend/run.py
@@ -2,11 +2,12 @@ import traceback
 
 from src import app
 from src.logger.logger import log
+from src.config import HOST
 
 
 if __name__ == "__main__":
     try:
         log("info", "Server startup", "Starting the API server.")
-        app.run()
+        app.run(host=HOST.split(":")[0])
     except Exception:
         log("critical", "Server startup failed", traceback.format_exc())
