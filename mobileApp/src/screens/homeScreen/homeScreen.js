cb3dbc84ad81feeb73aec34bf6c919271cd74ac2
Kamilsw <kamil@purpledecks.com>
2019-10-20 23:22:26 +0100

    create mock discover(previously aka library) screen

--- mobileApp/src/screens/homeScreen/homeScreen.js
+++ mobileApp/src/screens/homeScreen/homeScreen.js
@@ -1,18 +1,53 @@
 import React from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
+import { StyleSheet, Text, View, Image, FlatList } from "react-native"
 import GLOBALS from "../../utils/globalStrings";
 import styles from "./styles";
 import HeaderComponent from "../../components/headerComponent/headerComponent";
 import { SafeAreaView } from "react-navigation";
+import SearchComponent from "../../components/searchComponent/searchComponent";
+import songsData from "./sampleData";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class DummyScreen extends React.Component {
 
+  handleSongClick(item, index) {
+
+  }
+
+  renderSong({ item, index }) {
+    let songName = item.name
+    let authorName = item.author
+    let songImage = item.image
+    let playImage = require('../../assets/images/play.png')
+    return (
+      <TouchableOpacity style={styles.songContainer} onPress={() => this.handleSongClick(item, index)}>
+        {/* <Image style={styles.songImage} source={{uri: songImage}} /> */}
+        <Image style={styles.songImage} source={songImage} />
+        <Image style={styles.playImage} source={playImage} />
+        <View style={styles.songDetailsContainer}>
+          <Text style={styles.songNameText}>{songName}</Text>
+          <Text style={styles.authorNameText}>{authorName}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
-      <SafeAreaView style={{'backgroundColor': '#3D4044', 'flex': 1 }}>
-        <View style={{'backgroundColor': '#1B1E23', 'flex': 1 }}>
-          <HeaderComponent navigation={this.props.navigation}/>
-          <Text style={styles.testtext}>{"home screen"}</Text>
+      <SafeAreaView forceInset={{ bottom: 'never'}} style={{ 'backgroundColor': '#3D4044', 'flex': 1 }}>
+        <View style={{ 'backgroundColor': '#1B1E23', 'flex': 1 }}>
+          <HeaderComponent navigation={this.props.navigation} />
+          <View style={styles.container}>
+            <Text style={styles.titleText}>{"DISCOVER"}</Text>
+            <SearchComponent />
+            <FlatList 
+              style={styles.discoverFlatList}
+              data={songsData}
+              renderItem={this.renderSong.bind(this)}
+              keyExtractor={item => String(item.id)}
+              extraData={songsData}
+            />
+          </View>
           {/* <Text>{JSON.stringify(this.props, null, 2)}</Text> */}
 
         </View>
--- mobileApp/src/screens/homeScreen/styles.js
+++ mobileApp/src/screens/homeScreen/styles.js
@@ -2,13 +2,64 @@ import { StyleSheet, Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    testtext: {
-      color: 'white',
-    },
+  container: {
+    flex: 1,
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 24,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20,
+  },
+  discoverFlatList: {
+    marginTop: 20,
+  },
+  songContainer: {
+    width: width - 40,
+    height: 220,
+    marginBottom: 20,
+    marginLeft: 20,
+    backgroundColor: 'green',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playImage: {
+    resizeMode: 'contain',
+    width: 60,
+    height: 60,
+  },
+  songImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    borderRadius: 8,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    resizeMode: 'cover',
+  },
+  songDetailsContainer: {
+    position: 'absolute',
+    bottom: 18,
+    left: 20,
+    right: 20,
+  },
+  songNameText: {
+    color: '#FFF',
+    fontSize: 18,
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 1,
+    textShadowColor: '#666',
+  },
+  authorNameText: {
+    color: '#FFF',
+    fontSize: 14,
+    marginTop: 5,
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 1,
+    textShadowColor: '#666',
+  },
 })
