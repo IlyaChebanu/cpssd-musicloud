233bac67ee01b8eeb81d89bce33f65852caefced
Kamilsw <kamil@purpledecks.com>
2019-10-21 04:39:50 +0100

    remove console warn

--- mobileApp/src/screens/loginScreen/loginScreen.js
+++ mobileApp/src/screens/loginScreen/loginScreen.js
@@ -2,12 +2,11 @@ import React from "react";
 import { connect } from 'react-redux';
 import { ActionCreators } from '../../actions/index';
 import { bindActionCreators } from 'redux';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from "react-native"
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native"
 import GLOBALS from "../../utils/globalStrings";
 import styles from "./styles";
 import LoginInput from "../../components/loginInput/loginInput";
 import MultiPurposeButton from "../../components/multiPurposeButton/multiPurposeButton";
-const { width, height } = Dimensions.get('window');
 // import { loginUser } from "../../api/usersAPI";
 
 class LoginScreen extends React.Component {
@@ -25,7 +24,6 @@ class LoginScreen extends React.Component {
   }
 
   render() {
-    console.warn(height)
     var logoImage = require("../../assets/images/logo.png");
     var arrowback = require("../../assets/images/back_arrow.png");
     return (
