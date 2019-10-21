f93502cfb56b384a994ffc312b029132a3e38c91
Kamilsw <kamil@purpledecks.com>
2019-10-21 01:22:51 +0100

    create profileScreen

--- mobileApp/src/screens/profileScreen/profileScreen.js
+++ mobileApp/src/screens/profileScreen/profileScreen.js
@@ -5,17 +5,54 @@ import styles from "./styles";
 import HeaderComponent from "../../components/headerComponent/headerComponent";
 import { SafeAreaView } from "react-navigation";
 import ProfileComponent from "../../components/profileComponent/profileComponent";
+import ProfileSongs from "../../components/profileSongs/profileSongs";
+import ProfilePosts from "../../components/profilePosts/profilePosts";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class ProfileScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: 1,
+
+    }
+  }
+
+  handleSongsClick() {
+    this.setState({ activeTab: 1 })
+  }
+
+  handlePostsClick() {
+    this.setState({ activeTab: 2 })
+  }
 
   render() {
     return (
       <SafeAreaView forceInset={{ bottom: 'never'}} style={{'backgroundColor': '#3D4044', 'flex': 1 }}>
         <View style={{'backgroundColor': '#1B1E23', 'flex': 1 }}>
           <HeaderComponent navigation={this.props.navigation}/>
+          <View style={styles.profileTabs}>
+            <View style={styles.profileSongTab}>
+              {this.state.activeTab=== 1 && <View style={styles.activeTab} />}
+              <TouchableOpacity style={styles.tabClick} onPress={() => this.handleSongsClick()}>
+                <Text style={styles.tabTitleText}>{'Songs'}</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.profilePostTab}>
+              {this.state.activeTab=== 2 && <View style={styles.activeTab} />}
+              <TouchableOpacity style={styles.tabClick} onPress={() => this.handlePostsClick()}>
+                <Text style={styles.tabTitleText}>{'Posts'}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
           <View style={styles.container}>
             <Text style={styles.titleText}>{"PROFILE"}</Text>
             <ProfileComponent />
+            {this.state.activeTab === 1 ?
+            <ProfileSongs />
+            : <ProfilePosts />
+            }
           </View>
         </View>
       </SafeAreaView>
--- mobileApp/src/screens/profileScreen/styles.js
+++ mobileApp/src/screens/profileScreen/styles.js
@@ -12,4 +12,34 @@ export default StyleSheet.create({
     paddingRight: 20,
     paddingTop: 20,
   },
+  profileTabs: {
+    flexDirection: 'row',
+    height: 50,
+    backgroundColor: 'rgba(61,64,68,0.75)',
+  },
+  activeTab: {
+    backgroundColor: 'rgba(255,255,255,0.07)',
+    position: 'absolute',
+    borderBottomColor: '#FFF',
+    borderBottomWidth: 4,
+    bottom: 0,
+    top: 0,
+    right: 0,
+    left: 0,
+  },
+  tabClick: {
+    height: '100%',
+    width: width/2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profilePostTab: {
+    flex: 1,
+  },
+  profileSongTab: {
+    flex: 1,
+  },
+  tabTitleText: {
+    color: '#FFF'
+  },
 })
