9cae4dc16514f39a43e35c5941665585bc27649e
Rawd2020 <rawd2020personal@gmail.com>
2019-11-08 10:24:53 +0000

    109: Cleaned up config.

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,8 +3,8 @@
 # user & password field should be populated with the
 # username & password for MySQL located in the Password.kdbx file.
 MYSQL_CONFIG = {
-    'user': 'admin',
-    'password': '9xSN4xUupLNVmaY8cXmLcE2z9YBwYwnf',
+    'user': '',
+    'password': '',
     'host': 'musicloud.c3aguwab64mp.eu-west-1.rds.amazonaws.com',
     'database': 'musicloud_db',
     'raise_on_warnings': True
@@ -13,8 +13,8 @@ MYSQL_CONFIG = {
 # user & password field should be populated with the
 # username & password for Gmail located in the Password.kdbx file.
 SMTP_CONFIG = {
-    'user': 'AKIAQ6AXMYJ4ZNBXYUXU',
-    'password': 'BN4RQDDWAa8iVy267EV13lcb4IGprwtrar/Z5YIof+R1',
+    'user': '',
+    'password': '',
     'server': 'email-smtp.eu-west-1.amazonaws.com'
 }
 
@@ -22,7 +22,7 @@ HOST = "dcumusicloud.com:5000"
 
 # This should be set to the 'JWT_SECRET' value available in the Password.kdbx file.
 JWT_SECRET = (
-    "8cWFR7yAPGfGAexMYeFTsq4uvVmPfb4XY4Gp3wn3g6eBt4KTx7Z698duJy6P86XeUVHmQWdbdbAEnhj9JbiXGiUuk9hj8es7QzzQfPjM6ffVvXSxqLckA7sxfrjT2g2v6ywRisz2iynNYUbtrdrMspNMbRy3CxKF8nB3yaNr7WwQoxT7SLGE3RXmDU9LfAETjBJhRGHgaPSNvJTJhhasCEUEiijkGEv7vLrqSjxXFhqM7z5jJDXSLyu6RFuXY49S"
+    ""
 )
 
 AWS_CREDS = {
