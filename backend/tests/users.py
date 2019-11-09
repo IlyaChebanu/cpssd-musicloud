b398d7e57a0375c32f27d6aa686c4b1020a14fbc
Rawd2020 <rawd2020personal@gmail.com>
2019-11-09 00:12:23 +0000

    111: Added unit tests for /auth API calls

--- backend/tests/users.py
+++ backend/tests/users.py
@@ -7,19 +7,7 @@ from jwt.exceptions import InvalidSignatureError
 
 from ..src import app
 from ..src.models.errors import NoResults
-
-
-TEST_TOKEN = (
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOi0yLCJlbWFpbCI6InVz'
-    'ZXJuYW1lMkBmYWtlbWFpbC5ub3Nob3ciLCJ1c2VybmFtZSI6InVzZXJuYW1lMiIsI'
-    'nZlcmlmaWVkIjoxLCJyYW5kb21fdmFsdWUiOiJuQ1NpaFRUZ2ZiUUF0eGZLWFJNa2'
-    'ljRnh2WGJlQnVsRkp0aFd3VUVNdEpXWFRmTnN3TnpKSUt0YnpGb0t1anZMbUhkY0p'
-    'oQ1JPTWJuZVFwbEF1Q2RqQk5OZkxBSlFnVVdwWGFmR1hDbVRab0FRRW5YSVB1R0pz'
-    'bG12TXZmaWdmTmpnZUh5c1dEQW9CdHdISmFoYXlOUHVuRnZFZmdHb01XSUJkbkh1R'
-    'VNxRVpOQUVIdnhYdkNuQWNnZHpwTEVMbW5TWk9QSnBGYWxaaWJFUGtIVEdhR2NobW'
-    'hsQ1hUS29obm5lUk5FemNyTHpSemV5dnprc3NNRlVUZGVFdnpiS3UifQ.CQ0Rc-be'
-    'iTcw9vdLhHZBLNGoljbqO5OstvoyHxWiYIs'
-)
+from .constants import TEST_TOKEN
 
 
 class UserTests(unittest.TestCase):
