35f73198184f6b3d52c111d28995e9fe4a408a25
Kamilsw <kamil@purpledecks.com>
2019-11-14 05:59:32 +0000

    integrate get User Info api

--- mobileApp/src/components/profileComponent/profileComponent.js
+++ mobileApp/src/components/profileComponent/profileComponent.js
@@ -2,19 +2,41 @@ import React from "react"
 import { StyleSheet, Text, View, Image } from "react-native"
 import GLOBALS from "../../utils/globalStrings";
 import styles from "./styles";
+import { getUserInfo } from "../../api/usersAPI";
 export default class ProfileComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userDetails: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getUserDetails()
+  }
+
+  getUserDetails() {
+    getUserInfo(this.props.username, this.props.accessToken).then(response => {
+      if (isNaN(response)) {
+        this.setState({
+          userDetails: response
+        })
+      }
+    })
+  }
 
   render() {
     let profilePic = require('../../assets/images/profilePic.png')
+    let userData = this.state.userDetails
     return (
       <View style={styles.container}>
         <Image source={profilePic} />
         <View style={styles.statsContainer}>
-            <Text style={styles.profileText}><Text style={styles.profileNum}>{'300 '}</Text>{'followers'}{'\n'}
-            <Text style={styles.profileNum}>{'234 '}</Text>{'following'}{'\n'}
-            <Text style={styles.profileNum}>{'16 '}</Text>{'songs'}{'\n'}
-            <Text style={styles.profileNum}>{'12 '}</Text>{'posts'}{'\n'}
-            <Text style={styles.profileNum}>{'266 '}</Text>{'likes'}</Text>
+          <Text style={styles.profileText}><Text style={styles.profileNum}>{userData.followers}</Text>{' followers'}{'\n'}
+            <Text style={styles.profileNum}>{userData.following}</Text>{' following'}{'\n'}
+            <Text style={styles.profileNum}>{userData.songs}</Text>{' songs'}{'\n'}
+            <Text style={styles.profileNum}>{userData.posts}</Text>{' posts'}{'\n'}
+            <Text style={styles.profileNum}>{userData.likes}</Text>{' likes'}</Text>
         </View>
       </View>
     )
--- mobileApp/src/screens/profileScreen/profileScreen.js
+++ mobileApp/src/screens/profileScreen/profileScreen.js
@@ -49,7 +49,7 @@ class ProfileScreen extends React.Component {
             </View>
           </View>
             {this.state.activeTab === 1 ?
-            <ProfileSongs />
+            <ProfileSongs accessToken={this.props.token} username={this.props.username} />
             : <ProfilePosts accessToken={this.props.token} username={this.props.username} />
             }
         </View>
