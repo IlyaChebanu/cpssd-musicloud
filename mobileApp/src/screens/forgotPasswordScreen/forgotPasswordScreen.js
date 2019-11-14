2e3d362370282675c477bf1986f7eb21ffe57524
Kamilsw <kamil@purpledecks.com>
2019-11-14 02:58:51 +0000

    create forgotPasswordScreen

---
+++ mobileApp/src/screens/forgotPasswordScreen/forgotPasswordScreen.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { connect } from 'react-redux';
+import { ActionCreators } from '../../actions/index';
+import { bindActionCreators } from 'redux';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native"
+import GLOBALS from "../../utils/globalStrings";
+import styles from "./styles";
+import LoginInput from "../../components/loginInput/loginInput";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import MultiPurposeButton from "../../components/multiPurposeButton/multiPurposeButton";
+import PasswordInput from "../../components/passwordInput/passwordInput";
+
+const STATE_LOGIN_RESET_INITIAL = 1;
+const STATE_LOGIN_RESET_FINISH = 2;
+
+class ForgotPasswordScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      code: '',
+      password: '',
+      maskPassword: true,
+      maskPasswordRepeat: true,
+      screenState: [STATE_LOGIN_RESET_INITIAL],
+    }
+  }
+
+  handleBackClick() {
+    this.props.navigateBack()
+    this.props.setNewAccount(false)
+  }
+
+  handleResetClick() {
+    this.setState({ screenState: [STATE_LOGIN_RESET_FINISH] })
+  }
+
+  setEmailTextInput(text) {
+    this.setState({ email: text })
+  }
+
+  setCodeTextInput(text) {
+    this.setState({ code: text })
+  }
+
+  togglePasswordMask() {
+    this.setState({ maskPassword: !this.state.maskPassword });
+  }
+
+  togglePasswordRepeatMask() {
+    this.setState({ maskPasswordRepeat: !this.state.maskPasswordRepeat });
+  }
+
+  setPasswordTextInput(text) {
+    this.setState({ password: text })
+  }
+
+  setPasswordRepeatTextInput(text) {
+    this.setState({ passwordRepeat: text })
+  }
+
+  renderPasswordResetInitial() {
+    return (
+      <LoginInput
+            ref={ref => (this.emailInputName = ref)}
+            setText={this.setEmailTextInput.bind(this)}
+            style={{ "marginBottom": 1 }}
+            labelName={"Email"} />
+    )
+  }
+
+  renderPasswordResetFinal() {
+    return (
+      <View>
+        <LoginInput
+              ref={ref => (this.codeInputName = ref)}
+              setText={this.setCodeTextInput.bind(this)}
+              style={{ "marginBottom": 1 }}
+              labelName={"Code"} />
+        <PasswordInput
+            ref={ref => (this.passwordInputName = ref)}
+            togglePassword={this.togglePasswordMask.bind(this)}
+            maskPassword={this.state.maskPassword}
+            setText={this.setPasswordTextInput.bind(this)}
+            labelName={"New Password"} />
+        <PasswordInput
+            ref={ref => (this.passwordRepeatInputName = ref)}
+            togglePassword={this.togglePasswordRepeatMask.bind(this)}
+            maskPassword={this.state.maskPasswordRepeat}
+            setText={this.setPasswordRepeatTextInput.bind(this)}
+            labelName={"Repeat New Password"} />
+      </View>
+    )
+  }
+
+  render() {
+    var logoImage = require("../../assets/images/logo.png");
+    var arrowback = require("../../assets/images/back_arrow.png");
+    var topVector = require("../../assets/images/topVector.png");
+    var bottomVector = require("../../assets/images/bottomVector.png");
+    return (
+      <View style={styles.container}>
+        <Image style={styles.topVector} source={topVector} />
+        <KeyboardAwareScrollView>
+
+          <View style={styles.logoContainer}>
+            <TouchableOpacity onPress={() => this.handleBackClick()}>
+              <Image style={styles.arrowback} source={arrowback} />
+            </TouchableOpacity>
+            <Image style={styles.logo} source={logoImage} />
+          </View>
+
+          {this.state.screenState.includes(STATE_LOGIN_RESET_INITIAL) ? this.renderPasswordResetInitial() : null}
+          {this.state.screenState.includes(STATE_LOGIN_RESET_FINISH) ? this.renderPasswordResetFinal() : null}
+
+        </KeyboardAwareScrollView>
+        <Image style={styles.bottomVector}source={bottomVector} />
+        <MultiPurposeButton
+          handleButtonClick={this.handleResetClick.bind(this)}
+          style={styles.resetButton}
+          buttonName={"Reset Password"}
+        />
+      </View>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    newAccount: state.reg.newAccount,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(ActionCreators, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ForgotPasswordScreen);
---
+++ mobileApp/src/screens/forgotPasswordScreen/styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+export default StyleSheet.create({
+  container: {
+    // paddingTop: height > 600 ? 70 : 50,
+    backgroundColor: '#1B1E23',
+    flex: 1,
+  },
+  logoContainer: {
+    marginTop: height > 600 ? 100 : 80,
+    flexDirection: 'row',
+    marginLeft: 20,
+    alignItems: 'center',
+    marginBottom: height > 600 ? 50 : 20,
+  },
+  arrowback: {
+    width: 32,
+    height: 32,
+    resizeMode: 'contain',
+  },
+  logo: {
+    marginLeft: 20,
+    width: width-144,
+    resizeMode: 'contain'
+  },
+  topVector: {
+    position: 'absolute',
+    top: 0,
+    width: width,
+    resizeMode: 'cover',
+    zIndex: -1,
+  },
+  bottomVector: {
+    position: 'absolute',
+    bottom: -20,
+    width: width,
+    resizeMode: 'cover',
+    zIndex: -1,
+  },
+  resetButton: {
+    position: 'absolute',
+    bottom: height > 600 ? 100 : 50,
+  },
+})
