16e483d55d1629b0b58e1682793158fd86f9ecbb
Ilie Cebanu <cebanu5665@gmail.com>
2019-11-17 13:10:41 +0000

    Change API_URL

--- web/src/helpers/api.js
+++ web/src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `http://localhost:5000/api`;
+const API_URL = `http://dcumusicloud.com:5000/api`;
 
 export const login = (username, password) => {
   return axios.post(
