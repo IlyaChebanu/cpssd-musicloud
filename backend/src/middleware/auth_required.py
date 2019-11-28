3d0124520d6ec45d7c05102440ce436fa26a1131
Rawd2020 <rawd2020personal@gmail.com>
2019-11-28 17:41:04 +0000

    bug: Fixed bug where user sends  but no access_token string.

--- backend/src/middleware/auth_required.py
+++ backend/src/middleware/auth_required.py
@@ -13,7 +13,10 @@ def auth_required(return_user=False, return_token=False):
         def __auth_required(*args, **kwargs):
             if not request.headers.get("Authorization"):
                 return {"message": "Request missing access_token."}, 401
-            access_token = request.headers.get("Authorization").split(" ")[1]
+            try:
+                access_token = request.headers.get("Authorization").split(" ")[1]
+            except IndexError:
+                return {"message": "Bad access_token."}, 401
 
             try:
                 user = verify_and_refresh(access_token)
