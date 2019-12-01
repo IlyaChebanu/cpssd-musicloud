6a131463b5ad40c8e009120e149c546c4df3fb27
Rawd2020 <rawd2020personal@gmail.com>
2019-12-01 23:45:22 +0000

    Hotfix: Removed '.' prior to import statement.

--- backend/run.py
+++ backend/run.py
@@ -4,8 +4,8 @@ This is the kick-off function for the backend flask REST API server.
 import traceback
 from wsgiref.simple_server import make_server
 
-from .src import APP
-from .src.utils import log
+from src import APP  # pylint:disable=E0401
+from src.utils import log  # pylint:disable=E0401
 
 
 if __name__ == "__main__":
