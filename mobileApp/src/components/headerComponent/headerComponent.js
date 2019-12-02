09c32b4335238f71bb951a679ebce7b23ea30aea
Kamilsw <kamil@purpledecks.com>
2019-12-02 02:24:03 +0000

    add optional arrow prop to header

--- mobileApp/src/components/headerComponent/headerComponent.js
+++ mobileApp/src/components/headerComponent/headerComponent.js
@@ -4,12 +4,23 @@ import styles from "./styles";
 
 export default class HeaderComponent extends React.Component {
 
+    handleOnArrowClick() {
+        this.props.onArrowClick()
+    }
+
     render() {
         var logoImage = require("../../assets/images/logo.png");
         var menuImage = require("../../assets/images/menu.png");
+        var arrowBack = require("../../assets/images/back_arrow.png")
         return (
             <View style={styles.headerContainer}>
+                <View style={styles.logoContainer}>
+                {this.props.withArrow && 
+                <TouchableOpacity onPress={() => this.handleOnArrowClick()}>
+                    <Image style={styles.arrowBack} source={arrowBack} />
+                </TouchableOpacity>}
                 <Image style={styles.logo} source={logoImage} />
+                </View>
                 <TouchableOpacity style={styles.menuButton} onPress={this.props.navigation.openDrawer}>
                     <Image style={styles.menu} source={menuImage} />
                 </TouchableOpacity>
