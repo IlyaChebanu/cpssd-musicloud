0642b69251efdebd9cd44e033255c8d5f138a687
Kamilsw <kamil@purpledecks.com>
2019-12-06 00:15:14 +0000

    ios splash screen

--- mobileApp/src/App.js
+++ mobileApp/src/App.js
@@ -9,7 +9,7 @@ import React, { Fragment, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./Store";
 import Navigator, { middleware } from "./navigation/navigator";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import Orientation from 'react-native-orientation';
 import SplashScreen from 'react-native-splash-screen';
 
@@ -22,9 +22,12 @@ const App = () => {
   }, []);
 
   return (
-    <Provider store={store}>
-      <Navigator />
-    </Provider>
+    <Fragment>
+      <StatusBar barStyle="light-content" />
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
+    </Fragment>
   )
 }
 
