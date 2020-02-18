d082c097d212405d604c168f4357ac27771e595d
Ilie Cebanu <cebanu5665@gmail.com>
2020-02-18 08:59:33 +0000

    Refresh samples on track change

--- web/src/components/FileExplorer/FileExplorer.jsx
+++ web/src/components/FileExplorer/FileExplorer.jsx
@@ -1,54 +1,58 @@
-import React, { useState, useCallback, useMemo, memo } from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import PropTypes from "prop-types";
-import styles from "./FileExplorer.module.scss";
-import samplesIcon from "../../assets/icons/samples.svg";
-import instrumentsIcon from "../../assets/icons/instruments.svg";
-import { generatePresigned } from "../../helpers/api";
-import store from "../../store";
+import React, {
+  useState, useCallback, useMemo, memo,
+} from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AWS from 'aws-sdk';
+import styles from './FileExplorer.module.scss';
+import samplesIcon from '../../assets/icons/samples.svg';
+import instrumentsIcon from '../../assets/icons/instruments.svg';
+import { generatePresigned } from '../../helpers/api';
+import store from '../../store';
 import {
   setTrackAtIndex,
   setTracks,
   setSampleTime,
-  setSampleLoading
-} from "../../actions/studioActions";
-import { showNotification } from "../../actions/notificationsActions";
+  setSampleLoading,
+} from '../../actions/studioActions';
+import { showNotification } from '../../actions/notificationsActions';
 
-import { genId } from "../../helpers/utils";
-const FileExplorer = memo(props => {
+import { genId } from '../../helpers/utils';
+
+const FileExplorer = memo((props) => {
   const { studio, dispatch } = props;
   const [list, setList] = useState([]);
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState('');
 
   async function getFiles() {
-    const res = await generatePresigned("/");
-    var AWS = require("aws-sdk");
+    const res = await generatePresigned('/');
+    // var AWS = require("aws-sdk");
     const accessKey = res.data.signed_url.fields.AWSAccessKeyId;
     AWS.config.update({
       accessKeyId: accessKey,
-      secretAccessKey: "XVdgFyhjyhnqicDxxXZa9rLouFv5WQdXzXwxrP0u",
-      region: "eu-west-1"
+      secretAccessKey: 'XVdgFyhjyhnqicDxxXZa9rLouFv5WQdXzXwxrP0u',
+      region: 'eu-west-1',
     });
-    setUrl("https://dcumusicloudbucket.s3-eu-west-1.amazonaws.com/");
+    setUrl('https://dcumusicloudbucket.s3-eu-west-1.amazonaws.com/');
     const { user } = store.getState();
-    const s3ls = require("s3-ls");
+    const s3ls = require('s3-ls');
     const lister = s3ls({
-      bucket: "dcumusicloudbucket"
+      bucket: 'dcumusicloudbucket',
     });
 
-    const { files, _ } = await lister.ls("/audio/" + user.username);
+    const { files, _ } = await lister.ls(`/audio/${user.username}`);
     setList(files);
   }
 
   const addSample = useCallback(
-    name => {
+    (name) => {
       if (studio.tracks.length === 0) {
         dispatch(
           showNotification({
-            message: "Please add a track first",
-            type: "info"
-          })
+            message: 'Please add a track first',
+            type: 'info',
+          }),
         );
         return;
       }
@@ -56,20 +60,20 @@ const FileExplorer = memo(props => {
       console.log(url + name);
       const sampleState = {
         url: url + name,
-        name: name,
+        name,
         id: genId(),
         time: studio.currentBeat,
         track: studio.selectedTrack,
         fade: {
           fadeIn: 0,
-          fadeOut: 0
+          fadeOut: 0,
         },
         reverb: {
           wet: 1,
           dry: 1,
           cutoff: 0,
-          time: 0.3
-        }
+          time: 0.3,
+        },
       };
       track.samples.push(sampleState);
       dispatch(setTrackAtIndex(track, studio.selectedTrack));
@@ -77,64 +81,59 @@ const FileExplorer = memo(props => {
       dispatch(setSampleLoading(true));
       // dispatch(setTracks(studio.tracks));
     },
-    [studio, dispatch]
+    [studio.tracks, studio.selectedTrack, studio.currentBeat, url, dispatch],
   );
 
-  const Files = () => {
-    return (
-      <div>
-        <ul>
-          <li key={"Samples"} onClick={getFiles}>
-            {samplesIcon && (
-              <img
-                className={styles.icon}
-                src={samplesIcon}
-                alt="Samples icon"
-              />
-            )}
-            <p>{"Samples"}</p>
-          </li>
-        </ul>
-        {list.map((item, i) => {
-          return (
-            <li onClick={() => addSample(item)} className={styles.li}>
-              {item.split("/").pop()}
-            </li>
-          );
-        })}
-      </div>
-    );
-  };
+  const Files = () => (
+    <div>
+      <ul>
+        <li key="Samples" onClick={getFiles}>
+          {samplesIcon && (
+          <img
+            className={styles.icon}
+            src={samplesIcon}
+            alt="Samples icon"
+          />
+          )}
+          <p>Samples</p>
+        </li>
+      </ul>
+      {list.map((item, i) => (
+        <li onClick={() => addSample(item)} className={styles.li}>
+          {item.split('/').pop()}
+        </li>
+      ))}
+    </div>
+  );
 
   const instruments = [
-    { name: "Instruments", action: null, icon: instrumentsIcon }
+    { name: 'Instruments', action: null, icon: instrumentsIcon },
   ];
 
   const explorerItems = useMemo(
-    () =>
-      instruments.map(item => (
-        <li
-          key={item.name}
-          onMouseDown={() => {
-            if (item.action) item.action();
-          }}
-        >
-          {item.icon && (
-            <img
-              className={styles.icon}
-              src={item.icon}
-              alt="explorer item icon"
-            />
-          )}
-          <p>{item.name}</p>
-        </li>
-      )),
-    []
+    () => instruments.map((item) => (
+      <li
+        key={item.name}
+        onMouseDown={() => {
+          if (item.action) item.action();
+        }}
+      >
+        {item.icon && (
+        <img
+          className={styles.icon}
+          src={item.icon}
+          alt="explorer item icon"
+        />
+        )}
+        <p>{item.name}</p>
+      </li>
+    )),
+    [instruments],
   );
 
   return (
     <div
-      style={{ visibility: props.fileExplorerHidden ? "hidden" : "visible" }}
+      style={{ visibility: props.fileExplorerHidden ? 'hidden' : 'visible' }}
       className={styles.explorer}
     >
       <ul>
@@ -146,14 +145,14 @@ const FileExplorer = memo(props => {
 });
 
 FileExplorer.propTypes = {
-  fileExplorerHidden: PropTypes.bool.isRequired
+  fileExplorerHidden: PropTypes.bool.isRequired,
 };
 
-FileExplorer.displayName = "FileExplorer";
+FileExplorer.displayName = 'FileExplorer';
 
 const mapStateToProps = ({ studio }) => ({
   fileExplorerHidden: studio.fileExplorerHidden,
-  studio: studio
+  studio,
 });
 
 export default withRouter(connect(mapStateToProps)(FileExplorer));
