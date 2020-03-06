03e3beb0f8be40c29609b646e87e38ae142eecbc
Kamilsw1 <kamil.swituszak2@mail.dcu.ie>
2020-03-06 19:35:29 +0000

    save notifications settings to storage

--- mobileApp/src/utils/localStorage.js
+++ mobileApp/src/utils/localStorage.js
@@ -2,6 +2,11 @@ import AsyncStorage from '@react-native-community/async-storage';
 export const TOKEN_DATA_KEY = '@musicloud:authToken';
 export const USERNAME_DATA_KEY = '@musicloud:username';
 export const SETTINGS_PORTRAIT_DATA_KEY = '@musicloud:portrait';
+export const SETTINGS_NOTIFICATION_DATA_KEY = '@musicloud:notification';
+export const SETTINGS_NOTIFICATION_FOLLOW_DATA_KEY = '@musicloud:notificationFollow';
+export const SETTINGS_NOTIFICATION_POST_DATA_KEY = '@musicloud:notificationPost';
+export const SETTINGS_NOTIFICATION_SONG_DATA_KEY = '@musicloud:notificationSong';
+export const SETTINGS_NOTIFICATION_LIKE_DATA_KEY = '@musicloud:notificationLike';
 
 export async function readStorageData(dataKey) {
     try {
