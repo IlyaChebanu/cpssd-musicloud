34007fbfd3e10a389b64c44a06856905376823cd
Ilie Cebanu <cebanu5665@gmail.com>
2020-03-07 18:21:07 +0000

    Fix crash

--- web/src/middleware/setFadeCurve.js
+++ web/src/middleware/setFadeCurve.js
@@ -4,7 +4,7 @@ export default (gain, context, startTime, endTime, fadeIn, fadeOut) => {
   const fadeInEndTime = lerp(startTime, endTime, fadeIn);
   gain.gain.cancelScheduledValues(context.currentTime);
   gain.gain.setValueAtTime(
-    Math.min(1, map(context.currentTime, startTime, fadeInEndTime, 0, 1)),
+    Math.min(1, map(context.currentTime, startTime, fadeInEndTime, 0, 1)) || 0,
     startTime,
   );
   gain.gain.linearRampToValueAtTime(
@@ -14,7 +14,7 @@ export default (gain, context, startTime, endTime, fadeIn, fadeOut) => {
 
   const fadeOutBeginTime = lerp(startTime, endTime, 1 - fadeOut);
   gain.gain.setValueAtTime(
-    Math.min(1, 1 - map(context.currentTime, fadeOutBeginTime, endTime, 0, 1)),
+    Math.min(1, 1 - map(context.currentTime, fadeOutBeginTime, endTime, 0, 1)) || 1,
     fadeOutBeginTime,
   );
   gain.gain.linearRampToValueAtTime(0, endTime);
