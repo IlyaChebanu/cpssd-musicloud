47504df5417f0f637695cf2afa2e5a6bb9f83b3f
kr4sovsky <hermanis.krasovskis2@mail.dcu.ie>
2020-04-08 14:49:10 +0100

    fix eslint

--- web/src/components/TrackControls/TrackControls.jsx
+++ web/src/components/TrackControls/TrackControls.jsx
@@ -1,4 +1,6 @@
-import React, { memo, useCallback, useMemo, useState } from 'react';
+import React, {
+  memo, useCallback, useMemo, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import _ from 'lodash';
